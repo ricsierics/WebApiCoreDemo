cdee1b1eb7a9f377301345b6eb05bf44fc8b0d12
Enrico Magboo <enricomagboo@yahoo.com>
2017-02-17 08:14:51 +0800

    Commit: Initial commit

---
+++ src/WebApiCoreDemo/Models/TodoItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiCoreDemo.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Key { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+
+    }
+}
